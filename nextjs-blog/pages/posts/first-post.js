86431299d2754ea0347acb5c537ec222d8867dd5
tomi <bkc.tomi1979@gmail.com>
2020-08-09 13:19:26 +0900

    Assets, metadata, and CSS done

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,4 +1,6 @@
-import Link from "next/link";
+import Link   from "next/link";
+import Head   from "next/head";
+import Layout from "../../components/layout";
 
 /**
  * pages内のディレクトリ構成がそのままリンクパスになる。
@@ -8,12 +10,17 @@ import Link from "next/link";
 export default function FirstPost() {
     return (
         <>
-            <h1>First Page</h1>
-            <h2>
-                <Link href="/"> 
-                    <a>Back to home</a>
-                </Link>
-            </h2>
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+                <h1>First Page</h1>
+                <h2>
+                    <Link href="/"> 
+                        <a>Back to home</a>
+                    </Link>
+                </h2>
+            </Layout>
         </>
     );
 }
